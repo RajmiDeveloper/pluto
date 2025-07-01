43ffbd0241904211ffeb92833fcd0e4b494c202e
matias rajmilevich <matiasgrajmilevich@gmail.com>
2025-07-01 16:22:26 -0300

    feat: visor incompleted

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ require __DIR__ . '/clientes.php';
 require __DIR__ . '/productos.php';
 require __DIR__ . '/facturador.php';
 require __DIR__.'/editor.php';
+require __DIR__.'/facturas.php';
+require __DIR__.'/visor(incmplt).php';
 
 
 require __DIR__ . '/clientes.autocomplete.php';
