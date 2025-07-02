a354ec407ac5a64a71d118250c03dfb05da48d20
matias rajmilevich <matiasgrajmilevich@gmail.com>
2025-07-02 16:44:08 -0300

    feat: modal

--- routes/facturas.php
+++ routes/facturas.php
@@ -1,10 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/facturas', function() {
-    $facturas = DB::table('movimientos')->select('*')->get();
-    $clientes = DB::table('clientes')->select('*')->get();
-    return view('facturas', compact('clientes', 'facturas'));
+    $facturas = DB::table('movimientos')
+        // Ajusta el campo de unión según tu esquema:
+        ->join('clientes', 'movimientos.cliente_id', '=', 'clientes.id')
+        // Selecciono todos los campos de movimientos + el nombre del cliente
+        ->select(
+            'movimientos.*',
+            'clientes.nombre as cliente_nombre'
+        )
+        ->get();
+
+    return view('facturas', compact('facturas'));
 })->name('facturadas.index');
